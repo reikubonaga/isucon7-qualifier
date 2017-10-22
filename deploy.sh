d99047e22ae1a2d001891fa6174a3760bf0ca253
Nao Minami <south37777@gmail.com>
2017-10-22 17:05:37 +0900

    Add isubata.ruby.service

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,11 @@ echo 'Update config file...'
   # sudo cp "$HOME/my.conf" /etc/mysql/my.cnf
 echo 'Updateed config file!'
 
+echo 'Update service unit file...'
+  # -rw-r--r-- 1 root root 382 Oct 22 17:02 /etc/systemd/system/isubata.ruby.service
+  sudo cp "$HOME/isubata.ruby.service" /etc/systemd/system/isubata.ruby.service
+echo 'Updated service unit file!'
+
 if [ "$1" = "--bundle" ]; then
   echo 'Start bundle install...'
   cd /home/isucon/isubata/webapp/ruby
