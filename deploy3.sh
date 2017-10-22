e88c5dc487cefa66e2baf82985a5cd5a1bb78043
Nao Minami <south37777@gmail.com>
2017-10-22 15:51:51 +0900

    Add deploy3.sh

---
+++ deploy3.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# echo 'Rotate log file...'
+#   ./rotate.sh
+# echo 'Rotated log file!'
+
+echo 'Update config file...'
+  # sudo cp "$HOME/nginx.conf" /etc/nginx/nginx.conf
+  # sudo cp "$HOME/redis.conf" /etc/redis/redis.conf
+  # sudo cp "$HOME/my.conf" /etc/mysql/my.cnf
+echo 'Updateed config file!'
+
+if [ "$1" = "--bundle" ]; then
+  echo 'Start bundle install...'
+  cd /home/isucon/isubata/webapp/ruby
+  /home/isucon/local/ruby/bin/bundle install
+  cd "$HOME"
+  echo 'bundle install finished!'
+fi
+
+echo 'Restart services...'
+  # Save cache
+  # sudo systemctl restart mysql.service
+  sudo systemctl restart isubata.ruby.service
+echo 'Restarted!'
+
