ee9e4da31dc80c627f90f96e27561e80dac4ecb3
Nao Minami <south37777@gmail.com>
2017-10-22 15:08:15 +0900

    Remove unused pass

--- isubata/webapp/ruby/app.rb
+++ isubata/webapp/ruby/app.rb
@@ -329,19 +329,19 @@ class App < Sinatra::Base
   end
 
   # Deprecated
-  get '/icons/:file_name' do
-    file_name = params[:file_name]
-    statement = db.prepare('SELECT * FROM image WHERE name = ?')
-    row = statement.execute(file_name).first
-    statement.close
-    ext = file_name.include?('.') ? File.extname(file_name) : ''
-    mime = ext2mime(ext)
-    if !row.nil? && !mime.empty?
-      content_type mime
-      return row['data']
-    end
-    404
-  end
+  # get '/icons/:file_name' do
+  #   file_name = params[:file_name]
+  #   statement = db.prepare('SELECT * FROM image WHERE name = ?')
+  #   row = statement.execute(file_name).first
+  #   statement.close
+  #   ext = file_name.include?('.') ? File.extname(file_name) : ''
+  #   mime = ext2mime(ext)
+  #   if !row.nil? && !mime.empty?
+  #     content_type mime
+  #     return row['data']
+  #   end
+  #   404
+  # end
 
   private
 
